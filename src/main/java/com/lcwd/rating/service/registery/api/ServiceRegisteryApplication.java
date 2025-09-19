14d962d48412c5a97845a162c1082ae00181f36d
unknown <chauhannikhil20062005@gmail.com>
2025-09-19 19:47:21 +0530

    Initialized ServiceRegistery

---
+++ src/main/java/com/lcwd/rating/service/registery/api/ServiceRegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.lcwd.rating.service.registery.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisteryApplication.class, args);
+	}
+
+}
